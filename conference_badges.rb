11bf655bd990348813d88657d6259173a19fe4e2
Kevin Skilton <kevin.skilton@powerhrg.com>
2019-03-14 10:55:15 -0400

    passed, sorta

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
-# Write your code here.
+
+require "pry"
+
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  badges = []
+  names.each do |n|
+    i = "Hello, my name is #{n}."
+    badges << i
+  end
+  badges
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  room = 1
+  attendees.each do |n|
+    i = "Hello, #{n}! You'll be assigned to room #{room}!"
+    room += 1
+    room_assignments << i
+  end
+  room_assignments
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each do |badge|
+    puts badge
+  end
+  assign_rooms(attendees).each do |room_assignment|
+    puts room_assignment
+  end
+end
+
